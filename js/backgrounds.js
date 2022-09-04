e90d02950a8db50363eea300e1bf0268463585aa
Joinjoon001 <injoon00280@gmail.com>
2022-09-04 22:13:38 +0900

    v1.11

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -3,9 +3,11 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 //element를 js에서 만든다.
-const bgImage = document.getElementById("backscreen");
+const bgImage = document.querySelector(".backscreen");
 
 bgImage.src = `img/${chosenImage}`;
 
 /**  만든 element를 html의 body에 추가해준다. 
 document.body.appendChild(bgImage); */
+
+document.body.appendChild(bgImage);
